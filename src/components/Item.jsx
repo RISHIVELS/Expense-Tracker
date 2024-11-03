53361dbbb87d0c80bdcdf054aa48817a21b7349d
RISHIVELS <rishivel10@gmail.com>
2024-11-03 14:21:06 +0530

    all bugs fixed ready for production

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,9 +1,9 @@
 function Item({ expense }) {
+  const color = expense.amount > 0 ? "green" : "red";
+  console.log(color);
   return (
     <div
-      className={`flex justify-between mt-5 shadow-md p-2 border-r-8 border-${
-        expense.amount > 0 ? "green" : "red"
-      }-500 rounded-sm`}
+      className={`flex justify-between mt-5 shadow-md p-2 border-r-8 border-${color}-500 rounded-sm`}
     >
       <h2 className="text-gray-800  font-sans">{expense.text}</h2>
       <h2 className="font-semibold">
